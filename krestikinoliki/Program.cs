0a5e2f02f3bb0131aa96ede8b06b1cd80b686aa4
EgorPomidor <egorsdalekihgor@gmail.com>
2016-07-12 08:55:52 +0300

    qq

--- krestikinoliki/Program.cs
+++ krestikinoliki/Program.cs
@@ -6,9 +6,7 @@ using System.Threading.Tasks;
 
 namespace krestikinoliki
 {
-    //testifuguyjgkuhjkn
-    //jhgj,njknoijjliuglyukhjhli
-    //jhbliji;ojoik;
+    
     class Proga
     {
         private int[] massiv1 = new int[9];
@@ -154,8 +152,8 @@ namespace krestikinoliki
         Proga c;
         private Random _random = new Random();
 
-        public string vtoroihod, tretiihod, prosral;
-        public int chislo, rchislo, chislo2, dop;
+        public string vtoroihod, tretiihod, prosral, chetvertyihod, piatyihod;
+        public int chislo, h1, chislo2, dop, chislo3, chislo4, h2, h3, h4, h5;
         public Krestiki()
         {
             c = new Proga(this);
@@ -179,37 +177,37 @@ namespace krestikinoliki
         public int Rand7()
         {
             int b = _random.Next(1, 9);
-            while (b == rchislo | b == chislo)
+            while (b == h1 | b == chislo)
             {
                 b = _random.Next(1, 9);
             }
-            
             return b;
         }
         public int Rand7_1()
         {
             int b = _random.Next(1, 9);
-            while (b == rchislo | b == chislo | rchislo + chislo == 10)
+            while (b == h1 | b == chislo | h1 + b == 10)
             {
                 b = _random.Next(1, 9);
             }
-
             return b;
         }
         public int Rand1379()
         {
-            int b = _random.Next(1, 9);
-            while (b == 2 | b == 4 | b == 6 | b == 8 | b == 5)
-            {
-                b = _random.Next(1, 9);
-            }
-
-            return b;
+            if (chislo == 2)
+                return _random.Next(0, 1) == 0 ? 7 : 9;
+            if (chislo == 4)
+                return _random.Next(0, 1) == 0 ? 3 : 9;
+            if (chislo == 6)
+                return _random.Next(0, 1) == 0 ? 1 : 7;
+            if (chislo == 8)
+                return _random.Next(0, 1) == 0 ? 1 : 3;
+            return 1;
         }
         public int Rand1379_1()
         {
             int b = _random.Next(1, 9);
-            while (b == 2 | b == 4 | b == 6 | b == 8 | b == 5 | b == rchislo | b == chislo)
+            while (b == 2 | b == 4 | b == 6 | b == 8 | b == 5 | b == h1 | b == chislo)
             {
                 b = _random.Next(1, 9);
             }
@@ -217,131 +215,190 @@ namespace krestikinoliki
         }
         public int Rand1379_2()
         {
-            if (rchislo == 2)
+            if (h1 == 2)
                 return _random.Next(0, 1) == 0 ? 1 : 3;
-            if (rchislo == 4)
+            if (h1 == 4)
                 return _random.Next(0, 1) == 0 ? 1 : 7;
-            if (rchislo == 6)
+            if (h1 == 6)
                 return _random.Next(0, 1) == 0 ? 9 : 3;
-            if (rchislo == 8)
+            if (h1 == 8)
                 return _random.Next(0, 1) == 0 ? 7 : 9;
             return 1;
-
+        }
+        public int RandNeProtivUgol()
+        {
+            if (h1 == 1)
+                return _random.Next(0, 1) == 0 ? 7 : 3;
+            if (h1 == 3)
+                return _random.Next(0, 1) == 0 ? 1 : 9;
+            if (h1 == 7)
+                return _random.Next(0, 1) == 0 ? 9 : 1;
+            if (h1 == 9)
+                return _random.Next(0, 1) == 0 ? 7 : 3;
+            return 1;
         }
 
         public int Randnew()
         {
             int b = _random.Next(1, 9);
-            while (b == rchislo | b == chislo | b == chislo2)
+            while (b == h1 | b == chislo | b == chislo2 | b == h2)
             {
                 b = _random.Next(1, 9);
             }
 
             return b;
         }
-        
+        public int Randnew1()
+        {
+            int b = _random.Next(1, 9);
+            while (b == h1 | b == chislo | b == chislo2 | b == chislo3 | b == h2 | b == h3)
+            {
+                b = _random.Next(1, 9);
+            }
+
+            return b;
+        }
+        public int Randnew2()
+        {
+            for (int i = 0; i<9; i++)
+            {
+                if (massiv[i] != "х" | massiv[i] != "0") { return i; }
+            }
+             
+            return 1;
+        }
+
         public string[] massiv = new string[] { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
         public void X()
         {
             int x = hui();
-            rchislo = x;
+            h1 = x;
             massiv[x-1] = "х";
         }
     
         public void Hod2()
         {
+            massiv[chislo - 1] = "0";
             dop = 0;
-            int x = rchislo + chislo;
-            if (rchislo == 2 | rchislo == 4 | rchislo == 6 | rchislo == 8)
+            if (h1 == 2 | h1 == 4 | h1 == 6 | h1 == 8)
             {
                 if (vtoroihod == "1" | vtoroihod == "3" | vtoroihod == "7" | vtoroihod == "9")
                 {
-                    massiv[Rand7()-1] = "х";
-                    massiv[chislo - 1] = "0";
+                    h2 = Rand7() - 1;
+                    massiv[h2] = "х";
                 }
                 else if (vtoroihod == "5")
                 {
-                    massiv[Rand7_1() - 1] = "х";
-                    massiv[chislo - 1] = "0";
+                    h2 = Rand7_1() - 1;
+                    massiv[h2] = "х";
                 }
                 // противоположная сторона
-                else if (hui() + chislo == 10)
+                else if (h1 + chislo == 10)
                 {
-                    massiv[Rand7() - 1] = "х";
-                    massiv[chislo - 1] = "0";
+                    h2 = Rand7() - 1;
+                    massiv[h2] = "х";
                 }
                 else
                 {
                     dop = 1;
                     massiv[4] = "х";
-                    massiv[chislo - 1] = "0";
                 }
             }
-            else if (rchislo == 5)
+            else if (h1 == 5)
             {
                 if (vtoroihod == "1" | vtoroihod == "3" | vtoroihod == "7" | vtoroihod == "9")
                 {
-                    massiv[Rand7_1() - 1] = "х";
-                    massiv[chislo - 1] = "0";
+                    h2 = Rand7() - 1;
+                    massiv[h2] = "х";
                 }
                 else
                 {
-                    massiv[Rand1379()-1] = "х";
-                    massiv[chislo - 1] = "0";
+                    h2 = Rand1379() - 1;
+                    massiv[h2] = "х";
                 }
             }
             else
             {
                 if (vtoroihod == "2" | vtoroihod == "4" | vtoroihod == "6" | vtoroihod == "8")
                 {
-                    massiv[4] = "х";
-                    massiv[chislo - 1] = "0";
+                    h2 = RandNeProtivUgol();
+                    massiv[h2] = "х";
                 }
                 else if (vtoroihod == "5")
                 {
-                    massiv[10 - rchislo - 1] = "х";
-                    massiv[chislo - 1] = "0";
+                    h2 = 10 - h1 - 1;
+                    massiv[h2] = "х";
                 }
-                
-                else if (x == 10)
+                // противоположный угол
+                else if (h1 + chislo == 10)
                 {
-                    massiv[Rand1379_1()-1] = "х";
-                    massiv[chislo - 1] = "0";
+                    dop = 2;
+                    h2 = Rand1379_1() - 1;
+                    massiv[h2] = "х";
                 }
+                //не противоположный угол
                 else
                 {
-                    massiv[10 - chislo-1] = "х";
-                    massiv[chislo - 1] = "0";
+                    h2 = 10 - chislo - 1;
+                    massiv[h2] = "х";
                 }
             }
         }
         public void Hod3()
         {
-            
             massiv[chislo2 - 1] = "0";
 
             if (c.hz() == 33 & dop == 1)
             {
-                massiv[Rand1379_2()-1] = "х";
+                h3 = Rand1379_2() - 1;
+                massiv[h3] = "х";
+                return;
+            }
+            if (dop == 2)
+            {
+                h3 = 8 - h2;
+                massiv[h3] = "х";
                 return;
             }
 
             if (c.hz() == 33)
             {
-                massiv[Randnew() - 1] = "х";
+                h3 = Randnew() - 1;
+                massiv[h3] = "х";
             }
             else
             {
-                massiv[c.hz()] = "х";
+                h3 = c.hz();
+                massiv[h3] = "х";
             }
-           
-            
-
         }
+        public void Hod4()
+        {
+            massiv[chislo3 - 1] = "0";
+            
+            if (c.hz() == 33)
+            {
+                h4 = Randnew1() - 1;
+                massiv[h4] = "х";
+            }
+            else
+            {
+                h4 = c.hz();
+                massiv[h4] = "х";
+            }
+        }
+        public void Hod5()
+        {
+            massiv[chislo4 - 1] = "0";
+            h5 = Randnew2() - 1;
+            massiv[h5] = "х";
+        }
+
         public void iditenahui()
         {
+            int a = _random.Next(0, 2);
             string[] prosral1 = new string[] { "мне жаль(((", "ты лох азазазазаз", "просрал" };
-            prosral = prosral1[_random.Next(0, 2)];
+            prosral = prosral1[a];
             if (c.kill == 1)
             {
                 Console.WriteLine("{0}{1}{2}\n{3}{4}{5}\n{6}{7}{8}\n{9}", massiv[0], massiv[1], massiv[2], massiv[3], massiv[4], massiv[5], massiv[6], massiv[7], massiv[8], prosral);
@@ -349,10 +406,12 @@ namespace krestikinoliki
             else
             {
                 Console.WriteLine("{0}{1}{2}\n{3}{4}{5}\n{6}{7}{8}\nделайте ход", massiv[0], massiv[1], massiv[2], massiv[3], massiv[4], massiv[5], massiv[6], massiv[7], massiv[8]);
-                //Console.WriteLine(Rand1());
-                //Console.WriteLine(Rand7());
-                //Console.WriteLine(Rand7_1());
+                
             }
+        }
+        public void iditenahui1()
+        {
+                Console.WriteLine("{0}{1}{2}\n{3}{4}{5}\n{6}{7}{8}\nничья", massiv[0], massiv[1], massiv[2], massiv[3], massiv[4], massiv[5], massiv[6], massiv[7], massiv[8]);
         }
 
     }
@@ -383,8 +442,16 @@ namespace krestikinoliki
                 a.chislo2 = Convert.ToInt32(a.tretiihod);
                 a.Hod3();
                 a.iditenahui();
-                Console.WriteLine(a.prosral);
 
+                a.chetvertyihod = Console.ReadLine();
+                a.chislo3 = Convert.ToInt32(a.chetvertyihod);
+                a.Hod4();
+                a.iditenahui();
+
+                a.piatyihod = Console.ReadLine();
+                a.chislo4 = Convert.ToInt32(a.piatyihod);
+                a.Hod5();
+                a.iditenahui1();
             }
             
             Console.WriteLine("\n\n\n");
