f008cb6a819da65224312ee919623dcd064726f1
arbuziki <parfenovee@mail.ru>
2016-07-14 13:24:50 +0300

    микрофикс для Егорки

--- krestikinoliki/Program.cs
+++ krestikinoliki/Program.cs
@@ -259,12 +259,16 @@ namespace krestikinoliki
             List<int> a = new List<int>();
             for (int i = 0; i < 9; i++)
             {
-                if (massiv[i] != "х" | massiv[i] != "0") { a.Add(i); }
+                if (massiv[i] != "х" | massiv[i] != "0")
+                {
+                    a.Add(i);
+                }
             }
-            int b = _random.Next(0, 3);
+            int b = _random.Next(0, a.Count);
 
             return a[b];
         }
+
         public int Randnew2()
         {
             for (int i = 0; i<9; i++)
